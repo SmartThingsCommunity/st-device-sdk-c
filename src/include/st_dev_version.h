07a85737fe10dccfa0577d5709a04c75638fcea6
Eunchul Kim <35520523+chulspro@users.noreply.github.com>
2020-02-06 15:14:09 +0900

    Update core version to 1.0.16
    Signed-off-by: Eunchul Kim <35520523+chulspro@users.noreply.github.com>
    Signed-off-by: Donghoon Ryu <54927879+DongHoon-Ryu@users.noreply.github.com>

--- src/include/st_dev_version.h
+++ src/include/st_dev_version.h
@@ -26,7 +26,7 @@
 #define VER_MINOR	(0)
 
 /* patch: bug fix */
-#define VER_PATCH	(15)
+#define VER_PATCH	(16)
 
 /* External Macro for Apps, refer to linux's version.h */
 #define STDK_VERSION(a,b,c)	(((a) << 16) + ((b) << 8) + (c))
