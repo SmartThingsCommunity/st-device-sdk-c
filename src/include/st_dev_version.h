a49f055248f4b7adb7ef3bdd433e46accdcdf9e0
Yongsul Oh <46733179+YongsulOh@users.noreply.github.com>
2020-03-13 10:10:11 +0900

    Update core version to 1.1.4
    Signed-off-by: Yongsul Oh <46733179+YongsulOh@users.noreply.github.com>

--- src/include/st_dev_version.h
+++ src/include/st_dev_version.h
@@ -26,7 +26,7 @@
 #define VER_MINOR	(1)
 
 /* patch: bug fix */
-#define VER_PATCH	(3)
+#define VER_PATCH	(4)
 
 /* External Macro for Apps, refer to linux's version.h */
 #define STDK_VERSION(a,b,c)	(((a) << 16) + ((b) << 8) + (c))
