7d9daa4bdc9103f5e6655ec2e5164ea30d5641d4
Kwang-Hui Cho <54692798+Kwang-Hui@users.noreply.github.com>
2020-03-27 16:45:06 +0900

    test: seperate mock source file.
    seperate source code from TC_MOCK_functions.c to TC_MOCK_iot_bsp.c and TC_MOCK_iot_os.c

--- test/TC_MOCK_iot_bsp.c
+++ test/TC_MOCK_iot_bsp.c
@@ -22,8 +22,6 @@
 #include <string.h>
 #include <iot_error.h>
 #include <iot_bsp_wifi.h>
-#include <stdbool.h>
-#include <stdlib.h>
 
 iot_error_t __wrap_iot_bsp_wifi_get_mac(struct iot_mac *wifi_mac)
 {
@@ -80,97 +78,8 @@ uint16_t __wrap_iot_bsp_wifi_get_scan_result(iot_wifi_scan_result_t *scan_result
     return mock_scan_num;
 }
 
-
-#define MAX_MOCKED_IOT_OS_MALLOC_IN_TC 10
-static unsigned int _mock_malloc_failure_index;
-static bool _mock_iot_os_malloc_failure_at[MAX_MOCKED_IOT_OS_MALLOC_IN_TC];
-static bool _mock_iot_os_malloc_start;
-static bool _mock_detect_memory_leak;
-
-void set_mock_iot_os_malloc_failure_with_index(unsigned int index)
-{
-    assert_in_range(index, 0, MAX_MOCKED_IOT_OS_MALLOC_IN_TC - 1);
-    _mock_iot_os_malloc_failure_at[index] = true;
-    _mock_iot_os_malloc_start = true;
-}
-
-void set_mock_iot_os_malloc_failure()
-{
-    for (int i = 0; i < MAX_MOCKED_IOT_OS_MALLOC_IN_TC; i++) {
-        _mock_iot_os_malloc_failure_at[i] = true;
-    }
-    _mock_iot_os_malloc_start = true;
-}
-
-void do_not_use_mock_iot_os_malloc_failure()
-{
-    for (int i = 0; i < MAX_MOCKED_IOT_OS_MALLOC_IN_TC; i++) {
-        _mock_iot_os_malloc_failure_at[i] = false;
-    }
-    _mock_malloc_failure_index = 0;
-    _mock_iot_os_malloc_start = false;
-}
-
-void *__wrap_iot_os_malloc(size_t size)
-{
-    if (_mock_iot_os_malloc_start && _mock_iot_os_malloc_failure_at[_mock_malloc_failure_index]) {
-        if (++_mock_malloc_failure_index >= MAX_MOCKED_IOT_OS_MALLOC_IN_TC ) {
-            _mock_malloc_failure_index = MAX_MOCKED_IOT_OS_MALLOC_IN_TC - 1;
-        }
-        return NULL;
-    }
-    else if (_mock_iot_os_malloc_start && !_mock_iot_os_malloc_failure_at[_mock_malloc_failure_index]){
-        if (++_mock_malloc_failure_index >= MAX_MOCKED_IOT_OS_MALLOC_IN_TC ) {
-            _mock_malloc_failure_index = MAX_MOCKED_IOT_OS_MALLOC_IN_TC - 1;
-        }
-        if (_mock_detect_memory_leak)
-            return test_malloc(size);
-        else
-            return malloc(size);
-    } else {
-        if (_mock_detect_memory_leak)
-            return test_malloc(size);
-        else
-            return malloc(size);
-    }
-}
-
-void __wrap_iot_os_free(void* ptr)
-{
-    if (_mock_detect_memory_leak)
-        return test_free(ptr);
-    else
-        return free(ptr);
-}
-
-void *__wrap_iot_os_strdup(const char *src)
-{
-    if (_mock_detect_memory_leak) {
-        char *dest;
-        size_t size = strlen(src) + 1;
-
-        dest = test_malloc(size);
-        if (dest) {
-            memcpy(dest, src, size);
-        }
-        return dest;
-    } else {
-        return strdup(src);
-    }
-}
-
-void set_mock_detect_memory_leak(bool detect)
-{
-    _mock_detect_memory_leak = detect;
-}
-
 iot_error_t __wrap_iot_bsp_system_set_time_in_sec(const char* time_in_sec)
 {
     check_expected(time_in_sec);
     return IOT_ERROR_NONE;
-}
-
-void __wrap_iot_os_delay(unsigned int delay_ms)
-{
-    check_expected(delay_ms);
 }
