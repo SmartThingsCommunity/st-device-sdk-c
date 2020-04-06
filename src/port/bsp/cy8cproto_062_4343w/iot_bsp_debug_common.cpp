0fe51b657f7512a4c149c1eaaa4cf4de12a661be
Biman Paul <biman.paul@samsung.com>
2020-04-07 01:34:09 +0530

    port/bsp: implement debug for cypress

--- src/port/bsp/cy8cproto_062_4343w/iot_bsp_debug_common.cpp
+++ src/port/bsp/cy8cproto_062_4343w/iot_bsp_debug_common.cpp
@@ -19,33 +19,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "iot_bsp_debug.h"
 
+#include "iot_bsp_debug.h"
+#include "mbed.h"
+#include "platform/mbed_assert.h"
+#include "platform/mbed_debug.h"
+#include "platform/mbed_error.h"
+#include "platform/mbed_stats.h"
+
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_RESET "\x1b[0m"
 #define BUF_SIZE 512
+#define MAX_THREAD_INFO 10
+
+Mutex mutex;
+mbed_stats_heap_t heap_info;
+mbed_stats_stack_t stack_info[ MAX_THREAD_INFO ];
 
 void iot_bsp_dump(char* buf)
 {
-
+	/* TODO: implement dump */
 }
 
 void iot_bsp_debug(iot_debug_level_t level, const char* tag, const char* fmt, ...)
 {
+	char buf[BUF_SIZE] = {0,};
+	int ret;
+	va_list va;
 
+	va_start(va, fmt);
+	ret = vsnprintf(buf, BUF_SIZE, fmt, va);
+	va_end(va);
+
+	iot_bsp_dump(buf);
+	mutex.lock();
+	if (level == IOT_DEBUG_LEVEL_ERROR) {
+		printf(COLOR_RED "E %s: %s\n" COLOR_RESET, tag, buf);
+	} else if (level == IOT_DEBUG_LEVEL_WARN) {
+		printf(COLOR_YELLOW "W %s: %s\n" COLOR_RESET, tag, buf);
+	} else if (level == IOT_DEBUG_LEVEL_INFO) {
+		printf(COLOR_GREEN "I %s: %s\n" COLOR_RESET, tag, buf);
+	} else if (level == IOT_DEBUG_LEVEL_DEBUG) {
+		printf("D %s: %s\n", tag, buf);
+	} else {
+		printf("D %s: %s\n", tag, buf);
+	}
+	mutex.unlock();
 }
 
+/* TODO: get proper values of heap */
 static unsigned int _iot_bsp_debug_get_free_heap_size(void)
 {
-	return 0;
+	mbed_stats_heap_get(&heap_info);
+	return (heap_info.total_size - heap_info.current_size);
 }
 
 static unsigned int _iot_bsp_debug_get_minimum_free_heap_size(void)
 {
-	return 0;
+	mbed_stats_heap_get(&heap_info);
+	return (heap_info.total_size - heap_info.current_size);
 }
 
 static unsigned int _iot_bsp_debug_get_maximum_heap_size(void)
 {
-	return 0;
+	mbed_stats_heap_get(&heap_info);
+	return (heap_info.total_size);
 }
 
 void iot_bsp_debug_check_heap(const char* tag, const char* func, const int line, const char* fmt, ...)
@@ -70,3 +110,40 @@ void iot_bsp_debug_check_heap(const char* tag, const char* func, const int line,
 			_iot_bsp_debug_get_minimum_free_heap_size());
 }
 
+#if 0
+int test_debug_mbed()
+{
+	debug("\nThis message is from debug function");
+	debug_if(1,"\nThis message is from debug_if function");
+	debug_if(0,"\nSOMETHING WRONG!!! This message from debug_if function shouldn't show on bash");
+
+	printf("\nMemoryStats:");
+	mbed_stats_heap_get( &heap_info );
+	printf("\n\tBytes allocated currently: %d", heap_info.current_size);
+	printf("\n\tMax bytes allocated at a given time: %d", heap_info.max_size);
+	printf("\n\tCumulative sum of bytes ever allocated: %d", heap_info.total_size);
+	printf("\n\tCurrent number of bytes allocated for the heap: %d", heap_info.reserved_size);
+	printf("\n\tCurrent number of allocations: %d", heap_info.alloc_cnt);
+	printf("\n\tNumber of failed allocations: %d", heap_info.alloc_fail_cnt);
+
+	mbed_stats_stack_get( &stack_info[0] );
+	printf("\nCumulative Stack Info:");
+	printf("\n\tMaximum number of bytes used on the stack: %d", stack_info[0].max_size);
+	printf("\n\tCurrent number of bytes allocated for the stack: %d", stack_info[0].reserved_size);
+	printf("\n\tNumber of stacks stats accumulated in the structure: %d", stack_info[0].stack_cnt);
+
+	mbed_stats_stack_get_each( stack_info, MAX_THREAD_INFO );
+	printf("\nThread Stack Info:");
+	for(int i=0;i < MAX_THREAD_INFO; i++) {
+		if(stack_info[i].thread_id != 0) {
+			printf("\n\tThread: %d", i);
+			printf("\n\t\tThread Id: 0x%08X", stack_info[i].thread_id);
+			printf("\n\t\tMaximum number of bytes used on the stack: %d", stack_info[i].max_size);
+			printf("\n\t\tCurrent number of bytes allocated for the stack: %d", stack_info[i].reserved_size);
+			printf("\n\t\tNumber of stacks stats accumulated in the structure: %d", stack_info[i].stack_cnt);
+		}
+	}
+
+	printf("\nDone...\n\n");
+}
+#endif
