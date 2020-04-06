46b236306b4c82c3fa8d50a1b7de43284b07d21f
Biman Paul <biman.paul@samsung.com>
2020-04-07 01:55:02 +0530

    port/bsp: implement filesystem for cypress

--- src/port/bsp/cy8cproto_062_4343w/iot_bsp_fs_common.cpp
+++ src/port/bsp/cy8cproto_062_4343w/iot_bsp_fs_common.cpp
@@ -21,42 +21,229 @@
 #include "iot_bsp_fs.h"
 #include "iot_bsp_debug.h"
 
+
+/*
+ * Using LittleFileSystem as underlying storage
+ */
+
+#include "mbed.h"
+#include <stdio.h>
+#include <errno.h>
+
+#include "BlockDevice.h"
+#include "LittleFileSystem.h" /*TODO: check other filesystems */
+
+// This will take the system's default block device
+static BlockDevice *bd = BlockDevice::get_default_instance(); /* TODO: you can define your own block device */
+
+static LittleFileSystem fs("fs");
+
+/* TODO: identify the requirement for erase */
+/* To reformat the file system */
+#if 0
+static void erase_fs()
+{
+	IOT_INFO("Initializing the block device... ");
+	fflush(stdout);
+	int err = bd->init();
+	IOT_INFO("%s", (err ? "Fail :(" : "OK"));
+	if (err) {
+		IOT_ERROR("error: %s (%d)", strerror(-err), err);
+	}
+
+	IOT_INFO("Erasing the block device of size %d", bd->size());
+	fflush(stdout);
+	err = bd->erase(0, bd->size());
+	IOT_INFO("%s", (err ? "Fail :(" : "OK"));
+	if (err) {
+		IOT_ERROR("error: %s (%d)", strerror(-err), err);
+	}
+
+	IOT_INFO("Deinitializing the block device... ");
+	fflush(stdout);
+	err = bd->deinit();
+	IOT_INFO("%s", (err ? "Fail :(" : "OK"));
+	if (err) {
+		IOT_ERROR("error: %s (%d)", strerror(-err), err);
+	}
+}
+#endif
+
 iot_error_t iot_bsp_fs_init()
 {
+	//Reset Block Device
+	//	erase_fs();
+
+	// Try to mount the filesystem
+	IOT_INFO("Mounting the filesystem... ");
+	fflush(stdout);
+	int err = fs.mount(bd);
+	IOT_INFO("%s", (err ? "Fail :(" : "OK"));
+	if (err) {
+		// Reformat if we can't mount the filesystem
+		// this should only happen on the first boot
+		IOT_INFO("No filesystem found, formatting... ");
+		fflush(stdout);
+		err = fs.reformat(bd);
+		IOT_INFO("%s", (err ? "Fail :(" : "OK"));
+		if (err) {
+			IOT_ERROR("error: %s (%d)\n", strerror(-err), err);
+			return IOT_ERROR_INIT_FAIL;
+		}
+	}
+
 	return IOT_ERROR_NONE;
 }
 
 iot_error_t iot_bsp_fs_deinit()
 {
+	IOT_INFO("Unmounting... ");
+	fflush(stdout);
+	int err = fs.unmount();
+	IOT_INFO("%s", (err < 0 ? "Fail :(" : "OK"));
+	if (err < 0) {
+		IOT_ERROR("error: %s (%d)", strerror(-err), err);
+		return IOT_ERROR_INIT_FAIL;
+	}
 	return IOT_ERROR_NONE;
 }
 
 iot_error_t iot_bsp_fs_open(const char *filename, iot_bsp_fs_open_mode_t mode, iot_bsp_fs_handle_t *handle)
 {
+	FILE *f = NULL;
+
+	IOT_DEBUG("Open File: %s", filename);
+	if (mode == FS_READONLY) {
+		fflush(stdout);
+		f = fopen(filename, "r+");
+		IOT_DEBUG("%s", (!f ? "Fail :(" : "OK"));
+		if (!f) {
+			IOT_ERROR("error: %s (%d) (%s)", strerror(errno), -errno, filename);
+			return IOT_ERROR_FS_OPEN_FAIL;
+		}
+	} else {
+		fflush(stdout);
+		f = fopen(filename, "w+");
+		IOT_DEBUG("%s", (!f ? "Fail :(" : "OK"));
+		if (!f) {
+			IOT_ERROR("error: %s (%d) (%s)", strerror(errno), -errno, filename);
+			return IOT_ERROR_FS_OPEN_FAIL;
+		}
+	}
+
+	handle->fd = (int)f;
+	snprintf(handle->filename, sizeof(handle->filename), "%s", filename);
 	return IOT_ERROR_NONE;
 }
 
 iot_error_t iot_bsp_fs_open_from_stnv(const char *filename, iot_bsp_fs_handle_t *handle)
 {
-	return IOT_ERROR_NONE;
+	return iot_bsp_fs_open(filename, FS_READONLY, handle);
 }
 
 iot_error_t iot_bsp_fs_read(iot_bsp_fs_handle_t handle, char *buffer, unsigned int length)
 {
+	int ret = -1;
+
+	FILE *f = (FILE *) handle.fd;
+	if (!f) {
+		IOT_ERROR("File Not Open %s", handle.filename);
+		return IOT_ERROR_FS_NO_FILE;
+	}
+
+	fflush(stdout);
+	IOT_DEBUG("Reading From File: %s", handle.filename);
+	for (unsigned int i = 0; !feof(f) && i < length; i++) {
+		buffer[i] = fgetc(f);
+		ret = i;
+	}
+	buffer[ret] = 0x00;
+	if (ret <= 0) {
+		IOT_ERROR("Read Failed for %s [0x%x]", handle.filename, ret);
+		return IOT_ERROR_FS_READ_FAIL;
+	} else {
+		IOT_DEBUG("Bytes Read [%d]", ret);
+		//for (int i=0;i<ret; i++) {printf("%02x ", buffer[i]);} printf("\n");
+	}
+
 	return IOT_ERROR_NONE;
 }
 
 iot_error_t iot_bsp_fs_write(iot_bsp_fs_handle_t handle, const char *data, unsigned int length)
 {
+	int ret;
+	FILE *f = (FILE*) handle.fd;
+	if (!f) {
+		IOT_ERROR("ERROR: File Not Open %s", handle.filename);
+		return IOT_ERROR_FS_NO_FILE;
+	}
+
+	IOT_DEBUG("Seek to END");
+	fflush(stdout);
+	ret = fseek(f, 0, SEEK_END);
+	if (ret < 0) {
+		IOT_ERROR("Seek Failed for %s | %s [0x%x]", handle.filename,
+				strerror(errno), -errno);
+		return IOT_ERROR_FS_WRITE_FAIL;
+	}
+	IOT_DEBUG("%s", (ret < 0 ? "Fail :(" : "OK"));
+
+	IOT_DEBUG("Writing To File: %s", handle.filename);
+	fflush(stdout);
+	for (unsigned int i = 0; i < length; i++) {
+		fputc(data[i], f);
+		//printf("%02x ", data[i]);
+		ret = i + 1;
+	}
+//	printf("\n");
+
+	if (ret <= 0) {
+		IOT_ERROR("Write Failed for %s [0x%x]", handle.filename, ret);
+		return IOT_ERROR_FS_WRITE_FAIL;
+	} else {
+		IOT_DEBUG("Bytes Written [%d]", ret);
+	}
+
 	return IOT_ERROR_NONE;
 }
 
 iot_error_t iot_bsp_fs_close(iot_bsp_fs_handle_t handle)
 {
+	int ret;
+	FILE *f = (FILE *)handle.fd;
+	if (!f) {
+		IOT_ERROR("ERROR: File Not Open %s", handle.filename);
+		return IOT_ERROR_FS_NO_FILE;
+	}
+
+	fflush(stdout);
+	ret = fclose(f);
+	if (ret < 0) {
+		IOT_ERROR("Close Failed for %s [0x%x]", handle.filename, ret);
+		return IOT_ERROR_FS_CLOSE_FAIL;
+	}
+
 	return IOT_ERROR_NONE;
 }
 
 iot_error_t iot_bsp_fs_remove(const char *filename)
 {
+	FILE *f = fopen(filename, "r");
+	if (!f) {
+		IOT_ERROR("error: %s (%d) (%s)", strerror(errno), -errno, filename);
+		return IOT_ERROR_FS_NO_FILE;
+	}
+	fflush(stdout);
+	int close = fclose(f);
+	if (close < 0) {
+		IOT_ERROR("Close Failed for %s [0x%x]", filename, close);
+	}
+
+	int ret = remove(filename);
+	if (ret < 0) {
+		IOT_ERROR("Delete Failed for %s [0x%x]", filename, ret);
+		return IOT_ERROR_FS_REMOVE_FAIL;
+	}
+	IOT_WARN("Delete Successful for file %s", filename);
 	return IOT_ERROR_NONE;
 }
--- src/port/bsp/cy8cproto_062_4343w/iot_bsp_nv_data_common.cpp
+++ src/port/bsp/cy8cproto_062_4343w/iot_bsp_nv_data_common.cpp
@@ -1,6 +1,6 @@
 /* ***************************************************************************
  *
- * Copyright 2019 Samsung Electronics All Rights Reserved.
+ * Copyright 2019-2020 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,60 +16,56 @@
  *
  ****************************************************************************/
 
-
 #include "iot_bsp_nv_data.h"
 #include "iot_debug.h"
 
-const char* iot_bsp_nv_get_data_path(iot_nvd_t nv_type)
-{
+const char* iot_bsp_nv_get_data_path(iot_nvd_t nv_type) {
 	HIT();
-	IOT_WARN_CHECK((nv_type < 0 || nv_type > IOT_NVD_MAX), NULL, "Invalid args");
+	IOT_WARN_CHECK((nv_type < 0 || nv_type > IOT_NVD_MAX), NULL,
+			"Invalid args");
 
 	switch (nv_type) {
 
 	/* wifi prov data */
 	case IOT_NVD_WIFI_PROV_STATUS:
-		return "WifiProvStatus";
+		return "/fs/WifiProvStatus";
 	case IOT_NVD_AP_SSID:
-		return "IotAPSSID";
+		return "/fs/IotAPSSID";
 	case IOT_NVD_AP_PASS:
-		return "IotAPPASS";
+		return "/fs/IotAPPASS";
 	case IOT_NVD_AP_BSSID:
-		return "IotAPBSSID";
+		return "/fs/IotAPBSSID";
 	case IOT_NVD_AP_AUTH_TYPE:
-		return "IotAPAuthType";
-	/* wifi prov data */
+		return "/fs/IotAPAuthType";
 
 	/* cloud prov data */
 	case IOT_NVD_CLOUD_PROV_STATUS:
-		return "CloudProvStatus";
+		return "/fs/CloudProvStatus";
 	case IOT_NVD_SERVER_URL:
-		return "ServerURL";
+		return "/fs/ServerURL";
 	case IOT_NVD_SERVER_PORT:
-		return "ServerPort";
+		return "/fs/ServerPort";
 	case IOT_NVD_LOCATION_ID:
-		return "LocationID";
+		return "/fs/LocationID";
 	case IOT_NVD_ROOM_ID:
-		return "RoomID";
+		return "/fs/RoomID";
 	case IOT_NVD_LABEL:
-		return "Label";
-	/* cloud prov data */
-
+		return "/fs/Label";
 	case IOT_NVD_DEVICE_ID:
-		return "DeviceID";
+		return "/fs/DeviceID";
 
+	/* TODO: Get Manufacturer data */
 	/* stored in stnv partition (manufacturer data) */
 	case IOT_NVD_PRIVATE_KEY:
-		return "PrivateKey";
+		return "/rom/PrivateKey";
 	case IOT_NVD_PUBLIC_KEY:
-		return "PublicKey";
+		return "/rom/PublicKey";
 	case IOT_NVD_CA_CERT:
-		return "CACert";
+		return "/rom/CACert";
 	case IOT_NVD_SUB_CERT:
-		return "SubCert";
+		return "/rom/SubCert";
 	case IOT_NVD_SERIAL_NUM:
-		return "SerialNum";
-	/* stored in stnv partition (manufacturer data) */
+		return "/rom/SerialNum";
 
 	default:
 		return NULL;
