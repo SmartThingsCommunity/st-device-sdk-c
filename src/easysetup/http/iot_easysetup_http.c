970593490f713058f2ad0bd2f8042d880a62ee73
Kwang-Hui Cho <54692798+Kwang-Hui@users.noreply.github.com>
2020-06-02 09:06:46 +0900

    introduce STATIC_VARIABLE define for Unit TC
    We need to update static global variables for Unit TC (e.g. ref_step of easysetup)
    This define would be helpful for this purpose.

--- src/easysetup/http/iot_easysetup_http.c
+++ src/easysetup/http/iot_easysetup_http.c
@@ -34,7 +34,7 @@ static const char http_status_200[] = "HTTP/1.1 200 OK";
 static const char http_status_400[] = "HTTP/1.1 400 Bad Request";
 static const char http_status_500[] = "HTTP/1.1 500 Internal Server Error";
 static const char http_header[] = "\r\nServer: SmartThings Device SDK\r\nConnection: "CONNECTION_TYPE"\r\nContent-Type: application/json\r\nContent-Length: ";
-static int ref_step;
+STATIC_VARIABLE int ref_step;
 #if defined(CONFIG_STDK_IOT_CORE_EASYSETUP_HTTP_LOG_SUPPORT)
 static bool dump_enable;
 static char *log_buffer;
