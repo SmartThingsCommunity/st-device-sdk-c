b568f71ebafe7b719de099644f830d476201edff
Bart <grobebar@gmail.com>
2020-08-28 13:27:23 -0700

    updated code to raise exceptions

--- tools/qrgen/stdk-qrgen.py
+++ tools/qrgen/stdk-qrgen.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python3
 
-import qrcode
-import json
 import argparse
+import json
 import os
 from pathlib import Path
 
-__version__ = "1.0.0"
+import qrcode
+
+__version__ = "1.0.1"
 onboardingFile = "onboarding_config.json"
 deviceInfoFile = "device_info.json"
 
 
 def safeOpenJSONFile(file):
-    if (not os.path.isfile(file)):
-        print("ERROR: file does not exist %s" % (file))
-    else:
+    with open(file) as f:
         try:
-            return json.load(open(file))
+            return json.load(f)
         except:
-            print("ERROR: Unable to parse %s" % (file))
-    quit()
+            raise TypeError("Unable to parse %s" % (file))
 
 
 def safeReadJSON(key, data, file):
     if not key in data:
-        print("ERROR: %s not found in %s" % (key, file))
+        raise KeyError("%s not found in %s" % (key, file))
     elif data[key] is None or data[key] == "":
-        print("ERROR: %s value is empty in %s" % (key, file))
+        raise ValueError("%s value is empty in %s" % (key, file))
     else:
         return data[key]
-    quit()
 
 
 def main():
@@ -46,21 +43,25 @@ def main():
 
     args = parser.parse_args()
 
-    file = os.path.join(args.folder, onboardingFile)
-    data = safeOpenJSONFile(file)
-    safeReadJSON("onboardingConfig", data, file)
-    mnid = safeReadJSON("mnId", data["onboardingConfig"], file)
-    onboardingId = safeReadJSON("setupId", data["onboardingConfig"], file)
+    try:
+        file = os.path.join(args.folder, onboardingFile)
+        data = safeOpenJSONFile(file)
+        safeReadJSON("onboardingConfig", data, file)
+        mnid = safeReadJSON("mnId", data["onboardingConfig"], file)
+        onboardingId = safeReadJSON("setupId", data["onboardingConfig"], file)
 
-    file = os.path.join(args.folder, deviceInfoFile)
-    data = safeOpenJSONFile(file)
-    serialNumber = safeReadJSON("serialNumber", data["deviceInfo"], file)
+        file = os.path.join(args.folder, deviceInfoFile)
+        data = safeOpenJSONFile(file)
+        serialNumber = safeReadJSON("serialNumber", data["deviceInfo"], file)
 
-    qrUrl = "https://qr.samsungiots.com/?m=%s&s=%s&r=%s" % (mnid, onboardingId,
-                                                            serialNumber)
-    print("QR url: " + qrUrl)
-    img = qrcode.make(qrUrl)
-    img.save(serialNumber + '.png')
+        qrUrl = "https://qr.samsungiots.com/?m=%s&s=%s&r=%s" % (
+            mnid, onboardingId, serialNumber)
+        imgFP = serialNumber + '.png'
+        img = qrcode.make(qrUrl)
+        img.save(imgFP)
+        print("File:\t%s \nQR url:\t%s" % (imgFP, qrUrl))
+    except Exception as e:
+        print("ERROR: " + str(e))
 
 
 if __name__ == "__main__":
