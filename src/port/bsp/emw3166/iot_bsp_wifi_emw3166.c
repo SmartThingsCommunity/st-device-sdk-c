1001bc5dbe5bd5d5023e669bb0aadd10ebe82bf7
Kwang-Hui Cho <54692798+Kwang-Hui@users.noreply.github.com>
2021-09-28 19:46:04 +0900

    port: add interface for wifi event callback
    this patch adds interface for wifi event callback which returns IOT_ERROR_BAD_REQ
    
    Signed-off-by: Kwang-Hui Cho <54692798+Kwang-Hui@users.noreply.github.com>

--- src/port/bsp/emw3166/iot_bsp_wifi_emw3166.c
+++ src/port/bsp/emw3166/iot_bsp_wifi_emw3166.c
@@ -1,6 +1,6 @@
 /* ***************************************************************************
  *
- * Copyright 2020 Samsung Electronics All Rights Reserved.
+ * Copyright 2020-2021 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -356,4 +356,13 @@ iot_error_t iot_bsp_wifi_get_mac(struct iot_mac *wifi_mac)
 iot_wifi_freq_t iot_bsp_wifi_get_freq(void)
 {
 	return IOT_WIFI_FREQ_2_4G_ONLY;
+}
+
+iot_error_t iot_bsp_wifi_register_event_cb(iot_bsp_wifi_event_cb_t cb)
+{
+	return IOT_ERROR_BAD_REQ;
+}
+
+void iot_bsp_wifi_clear_event_cb(void)
+{
 }
