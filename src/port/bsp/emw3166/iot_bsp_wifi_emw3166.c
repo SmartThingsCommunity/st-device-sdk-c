f5901b3f90c76b4c14e72ac1fba570c0eed113b3
Kwang-Hui Cho <kwanghui.cho@samsung.com>
2022-01-28 11:41:10 +0900

    port:bsp: fix missing semicolon

--- src/port/bsp/emw3166/iot_bsp_wifi_emw3166.c
+++ src/port/bsp/emw3166/iot_bsp_wifi_emw3166.c
@@ -371,7 +371,7 @@ iot_wifi_auth_mode_bits_t iot_bsp_wifi_get_auth_mode(void)
 {
 	iot_wifi_auth_mode_bits_t supported_mode_bits = IOT_WIFI_AUTH_MODE_BIT_ALL;
 	supported_mode_bits ^= IOT_WIFI_AUTH_MODE_BIT(IOT_WIFI_AUTH_WPA2_ENTERPRISE);
-	supported_mode_bits ^= IOT_WIFI_AUTH_MODE_BIT(IOT_WIFI_AUTH_WPA3_PERSONAL)
+	supported_mode_bits ^= IOT_WIFI_AUTH_MODE_BIT(IOT_WIFI_AUTH_WPA3_PERSONAL);
 
 	return supported_mode_bits;
 }
